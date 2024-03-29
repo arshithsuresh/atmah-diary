9bf7caede2c1668186db3cdc3120d9f7101dca41
Arshith Suresh <arshith007@gmail.com>
2024-03-29 21:09:48 +0530

    ATMAH-#1 Creating diary UI Components

---
+++ atmah-diary/src/app/views/diary/diary/diary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atmah-diary',
+  standalone: true,
+  imports: [],
+  templateUrl: './diary.component.html',
+  styleUrl: './diary.component.scss',
+})
+export class DiaryComponent {}
