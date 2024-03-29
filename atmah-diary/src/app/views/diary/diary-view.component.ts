dca097f3bd2b3f772bbd7fdd90d56367b150fbca
Arshith Suresh <arshith007@gmail.com>
2024-03-30 01:22:43 +0530

    ATMAH-#1 Prototype UI- Added Styles and Fonts

--- atmah-diary/src/app/views/diary/diary-view.component.ts
+++ atmah-diary/src/app/views/diary/diary-view.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FullPageContainerComponent } from '../../components/full-page-container/full-page-container.component';
+import { DiaryComponent } from './diary/diary.component';
 
 @Component({
   selector: 'view-diary',
   standalone: true,
-  imports: [],
+  imports: [FullPageContainerComponent, DiaryComponent],
   templateUrl: './diary-view.component.html',
   styleUrl: './diary-view.component.scss',
 })
-export class DiaryComponent {}
+export class DiaryViewComponent {}
