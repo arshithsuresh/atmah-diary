a05a73f69435f6e99d534de2fc7e57991d0c1b33
Arshith Suresh <arshith007@gmail.com>
2024-03-31 18:10:05 +0530

    ATMAH-#1: Adding title

---
+++ atmah-diary/src/app/components/chapter-title/chapter-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atmah-chapter-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './chapter-title.component.html',
+  styleUrl: './chapter-title.component.scss'
+})
+export class ChapterTitleComponent {
+
+}
--- atmah-diary/src/app/views/diary/diary-view.component.ts
+++ atmah-diary/src/app/views/diary/diary-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FullPageContainerComponent } from '../../components/full-page-container/full-page-container.component';
 import { DiaryComponent } from './diary/diary.component';
+import { ChapterTitleComponent } from '../../components/chapter-title/chapter-title.component';
 
 @Component({
   selector: 'view-diary',
   standalone: true,
-  imports: [FullPageContainerComponent, DiaryComponent],
+  imports: [FullPageContainerComponent, DiaryComponent, ChapterTitleComponent],
   templateUrl: './diary-view.component.html',
   styleUrl: './diary-view.component.scss',
 })
