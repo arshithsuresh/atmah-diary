3aa7375910a90e8af0d14cee8ce0447b742ba22a
Arshith Suresh <arshith007@gmail.com>
2024-09-22 01:25:36 +0530

    Created login and dashboard views

---
+++ atmah-diary/src/app/views/login/login.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginViewComponent } from './login-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
