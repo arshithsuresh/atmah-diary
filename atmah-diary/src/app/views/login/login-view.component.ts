5605b4dd9d665462ef7ef354edf2f09c2fc462fa
Arshith Suresh <arshith007@gmail.com>
2024-11-03 01:34:33 +0530

    install gsap

--- atmah-diary/src/app/views/login/login-view.component.ts
+++ atmah-diary/src/app/views/login/login-view.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'atmah-login-view',
   templateUrl: './login-view.component.html',
-  styleUrl: './login-view.component.scss'
+  styleUrl: './login-view.component.scss',
 })
-export class LoginViewComponent {
-
-}
+export class LoginViewComponent {}
