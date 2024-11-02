09c8f8a6b0bd755f1086409a80d01e299748aa0c
Arshith Suresh <arshith007@gmail.com>
2024-11-03 01:22:41 +0530

    Init login page

--- atmah-diary/src/app/views/login/login.module.ts
+++ atmah-diary/src/app/views/login/login.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginViewComponent } from './login-view.component';
 import { loginRoutes } from './login.routes';
 import { provideRouter } from '@angular/router';
+import { FullPageContainerComponent } from '../../components/full-page-container/full-page-container.component';
 
 @NgModule({
   declarations: [LoginViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FullPageContainerComponent],
   providers: [provideRouter(loginRoutes)],
 })
 export class LoginModule {}
