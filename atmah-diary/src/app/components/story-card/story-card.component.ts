c9d267a9f869e7194083cc302119a28eb29e9ad2
Arshith Suresh <arshith007@gmail.com>
2024-12-24 01:41:18 +0530

    added story card component

---
+++ atmah-diary/src/app/components/story-card/story-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atmah-story-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './story-card.component.html',
+  styleUrl: './story-card.component.scss',
+})
+export class StoryCardComponent {}
--- atmah-diary/src/app/views/dashboard/dashboard.module.ts
+++ atmah-diary/src/app/views/dashboard/dashboard.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardViewComponent } from './dashboard-view.component';
 import { provideRouter } from '@angular/router';
 import { dashboardRoutes } from './dashboard.routes';
+import { StoryCardComponent } from '../../components/story-card/story-card.component';
 
 @NgModule({
   declarations: [DashboardViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, StoryCardComponent],
   providers: [provideRouter(dashboardRoutes)],
 })
 export class DashboardModule {}
