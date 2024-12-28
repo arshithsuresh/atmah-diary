7cc49f0551ecf45d8ee94ad2be3249837a563bb0
Arshith Suresh <arshith007@gmail.com>
2024-12-28 18:25:01 +0530

    Dashboard initial

--- atmah-diary/src/app/views/dashboard/dashboard.module.ts
+++ atmah-diary/src/app/views/dashboard/dashboard.module.ts
@@ -4,10 +4,11 @@ import { DashboardViewComponent } from './dashboard-view.component';
 import { provideRouter } from '@angular/router';
 import { dashboardRoutes } from './dashboard.routes';
 import { StoryCardComponent } from '../../components/story-card/story-card.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @NgModule({
   declarations: [DashboardViewComponent],
-  imports: [CommonModule, StoryCardComponent],
+  imports: [CommonModule, StoryCardComponent, HeaderComponent],
   providers: [provideRouter(dashboardRoutes)],
 })
 export class DashboardModule {}
