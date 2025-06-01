362d1be84cafe28f3f3def7d4ed298e1f6b031f6
Arshith Suresh <arshith007@gmail.com>
2025-06-01 17:29:02 +0530

    backend: Update deps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,15 @@
 import router from "./routes/routes";
 import express, { Express } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const myRoute = router;
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
 app.use("/api/v1", myRoute);
 
 export default app;
